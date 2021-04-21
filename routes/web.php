dbda71f9ba03a9cde6be78872f058ab32c9adfaa
Angel Flores <angelflo@usc.edu>
2021-04-20 22:06:15 -0700

    added procfile

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+if (env('APP_ENV') !== 'local') {
+    URL::forceScheme('https');
+}
+
