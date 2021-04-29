a8302f0736c14bfc56ef3baeb3b2ad7bcc1cd26b
Angel Flores <angelflo@usc.edu>
2021-04-28 22:57:47 -0700

    litle mobile friendly

--- resources/views/characters/random.blade.php
+++ resources/views/characters/random.blade.php
@@ -8,9 +8,20 @@
             width: 100%;
             height: auto;
         }
+        .god_card{
+            width: 35%; 
+            height:50%;
+        }
+
+        @media (max-width: 775px){
+            .god_card{
+                width: max-content;
+                height: auto;
+            }
+        }
   </style>
     <div class = "d-flex flex-row flex-wrap text-center justify-content-center">
-        <div style="background-color:#6E6E6E; width: 25%; height:20%" class = "p-5 m-5">
+        <div style="background-color:#6E6E6E;" class = "p-5 m-5 god_card">
                 <h1 style="font-size:2vw;" >{{ $name }}</h1>
                 <img src="{{ URL::to('/') }}/images/{{ preg_replace('/\s+/', '-', strtolower($name)) }}.jpg" class="image_custom" alt="Image" />
                 <p style="font-size:1vw;">{{ $pantheon }} {{ $damage }} {{ $type }}</p>
