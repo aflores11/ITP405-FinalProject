0c6bbe31f73037a1ec5c4e0231c3b3930d51c16d
Angel Flores <angelflo@usc.edu>
2021-04-28 23:50:16 -0700

    modified gods and random css

--- resources/views/characters/random.blade.php
+++ resources/views/characters/random.blade.php
@@ -12,21 +12,33 @@
             width: 35%; 
             height:50%;
         }
+        .text_display{
+            font-size:2vw;
+        }
+        .text_display2{
+            font-size:1vw;
+        }
 
         @media (max-width: 775px){
             .god_card{
                 width: max-content;
                 height: auto;
             }
+            .text_display{
+                font-size:5vw;
+            }
+            .text_display2{
+                font-size:3vw;
+            }
         }
   </style>
     <div class = "d-flex flex-row flex-wrap text-center justify-content-center">
         <div style="background-color:#6E6E6E;" class = "p-5 m-5 god_card">
-                <h1 style="font-size:2vw;" >{{ $name }}</h1>
+                <h1 class="text_display" >{{ $name }}</h1>
                 <img src="{{ URL::to('/') }}/images/{{ preg_replace('/\s+/', '-', strtolower($name)) }}.jpg" class="image_custom" alt="Image" />
-                <p class="mt-2" style="font-size:1vw;">Pantheon: {{ $pantheon }}</p>
-                <p style="font-size:1vw;">Damage Type: {{ $damage }}</p>
-                <p style="font-size:1vw;">Default Role: {{ $type }}</p>
+                <p class="mt-2 text_display2">Pantheon: {{ $pantheon }}</p>
+                <p class="text_display2">Damage Type: {{ $damage }}</p>
+                <p class="text_display2">Default Role: {{ $type }}</p>
         </div>
     </div>
 @endsection
--- resources/views/characters/show.blade.php
+++ resources/views/characters/show.blade.php
@@ -13,21 +13,35 @@
             height:50%;
         }
 
+        .text_display{
+            font-size:1.5vw;
+        }
+        .text_display2{
+            font-size:1vw;
+        }
+
+
         @media (max-width: 775px){
             .god_card{
                 width: max-content;
                 height: auto;
+            }
+            .text_display{
+                font-size:5vw;
+            }
+            .text_display2{
+                font-size:3vw;
             }
         }
     </style>
     <div class = "d-flex flex-row flex-wrap text-center justify-content-center">
         @foreach ($gods as $god)
             <div style="background-color:#6E6E6E;" class = "p-5 mt-5 ms-2 me-2 god_card">
-                <h1 style="font-size:1.5vw;" >{{ $god->name }}</h1>
+                <h1 class="text_display" >{{ $god->name }}</h1>
                 <img src="{{ URL::to('/') }}/images/{{ preg_replace('/\s+/', '-', strtolower(preg_replace('/\'/', '', $god->name))) }}.jpg" class="image_custom" alt="Image" />
-                <p class="mt-2" style="font-size:1vw;">{{ $god->pantheon->name }}</p>
-                <p style="font-size:1vw;">{{ $god->damage->damage_type }}</p>
-                <p style="font-size:1vw;">{{ $god->type->role_type }}</p>
+                <p class="mt-2 text_display2">{{ $god->pantheon->name }}</p>
+                <p class="text_display2">{{ $god->damage->damage_type }}</p>
+                <p class="text_display2">{{ $god->type->role_type }}</p>
             </div>   
         @endforeach
     </div>
